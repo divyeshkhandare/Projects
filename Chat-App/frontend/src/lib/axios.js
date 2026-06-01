e342067c1686f95b29d50e9f558299aaa4b01447
Divyesh Khandare <divyeshkhandare@gmail.com>
2026-06-01 12:00:07 +0530

    Updaated

--- Chat-App/frontend/src/lib/axios.js
+++ Chat-App/frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://projects-1-hhp3.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8090/api" : "https://projects-1-hhp3.onrender.com/api",
   withCredentials: true,
 })
--- Chat-App/frontend/src/store/useAuthStore.js
+++ Chat-App/frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "https://projects-1-hhp3.onrender.com"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8090" : "https://projects-1-hhp3.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
