78960b4dabd0650c49e903e00d32c901f9993592
Divyesh Khandare <divyeshkhandare@gmail.com>
2026-06-01 11:50:02 +0530

    Updaated

--- Chat-App/backend/src/index.js
+++ Chat-App/backend/src/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://projects-one-phi.vercel.app",
     credentials: true,
   })
 );
